5e88ae1691c4d26e6cda95d59a3c05580804fb1d
Yannik Behr <yannik@yanmail.de>
2025-08-01 02:15:24 +0000

    add explicit chunking

--- src/tonik/xarray2zarr.py
+++ src/tonik/xarray2zarr.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import pandas as pd
 import xarray as xr
 try:
     from zarr.errors import PathNotFoundError
@@ -13,7 +14,42 @@ from .utils import merge_arrays
 logger = logging.getLogger(__name__)
 
 
-def xarray2zarr(xds: xr.Dataset, path: str, mode: str = 'a', group='original'):
+def get_encoding(coords: xr.core.coordinates.DatasetCoordinates, chunks: int = 1) -> dict:
+    """
+    Determine the chunk size for the datetime dimension. Other dimensions are assumed to be 
+    small enough to not require chunking.
+
+    Parameters
+    ----------
+    coords : xr.core.coordinates.DatasetCoordinates
+        Coordinates of the dataset.
+    chunks : int, optional
+        Number of chunks in days to divide the datetime dimension into, by default 1.
+    """
+    if 'datetime' not in coords:
+        raise ValueError(
+            "Datetime coordinate not found in dataset coordinates.")
+
+    dt = pd.infer_freq(coords['datetime'])
+    if dt is None:
+        dates = pd.to_datetime(coords['datetime'].values)
+        dt = dates.diff().median()
+    try:
+        chunklength = int(pd.Timedelta('%dD' % chunks) / pd.Timedelta(dt))
+    except ValueError:
+        # pd.infer_freq leaves out the unit if it's 1
+        chunklength = int(pd.Timedelta('1D') / pd.Timedelta("1%s" % dt))
+    encoding = {}
+    for key in coords:
+        if key == 'datetime':
+            encoding['datetime'] = {'chunks': (chunklength,)}
+        else:
+            encoding[key] = {'chunks': (coords[key].size,)}
+    return encoding
+
+
+def xarray2zarr(xds: xr.Dataset, path: str, mode: str = 'a', group='original',
+                chunks: int = 1) -> None:
     """
     Write xarray dataset to zarr files.
 
@@ -27,6 +63,8 @@ def xarray2zarr(xds: xr.Dataset, path: str, mode: str = 'a', group='original'):
         Write mode, by default 'a'.
     group : str, optional
         Group name, by default 'original'
+    chunks : int, optional
+        Chunk size as the number of days.
 
     Returns
     -------
@@ -34,18 +72,21 @@ def xarray2zarr(xds: xr.Dataset, path: str, mode: str = 'a', group='original'):
     """
     for feature in xds.data_vars.keys():
         fout = os.path.join(path, feature + '.zarr')
+        encoding = get_encoding(xds[feature].coords, chunks)
         if not os.path.exists(fout) or mode == 'w':
             xds[feature].to_zarr(
-                fout, group=group, mode='w')
+                fout, group=group, mode='w',
+                encoding=encoding)
         else:
             try:
-                xds_existing = xr.open_zarr(fout, group=group)
+                xds_existing = xr.open_zarr(fout, group=group, chunks={})
             except (PathNotFoundError, FileNotFoundError):
-                xds[feature].to_zarr(fout, group=group, mode='a')
+                xds[feature].to_zarr(
+                    fout, group=group, mode='a')
                 continue
             if xds_existing.datetime[0] > xds.datetime[0] or xds_existing.datetime[-1] > xds.datetime[-1]:
                 xda_new = merge_arrays(xds_existing[feature], xds[feature])
-                xda_new.to_zarr(fout, group=group, mode='w')
+                xda_new.to_zarr(fout, group=group, mode='w', encoding=encoding)
             else:
                 try:
                     overlap = xds_existing.datetime.where(
@@ -64,4 +105,5 @@ def xarray2zarr(xds: xr.Dataset, path: str, mode: str = 'a', group='original'):
                     logger.error(msg)
                     # remove duplicate datetime entries
                     xda_new = merge_arrays(xds_existing[feature], xds[feature])
-                    xda_new.to_zarr(fout, group=group, mode='w')
+                    xda_new.to_zarr(fout, group=group,
+                                    mode='w', encoding=encoding)
